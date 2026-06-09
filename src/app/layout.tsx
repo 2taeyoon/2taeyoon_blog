eef4b034c124c4f897993edea2cd1c9fc7eaf6ac
2taeyoon <2tae@kakao.com>
2026-06-10 02:39:53 +0900

    refactor: update import paths for BlogHeaderWrapper and BlogAideWrapper components to improve project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import BlogHeaderWrapper from "@/components/blog/layout/BlogHeaderWrapper";
-import BlogAideWrapper from "@/components/blog/layout/BlogAideWrapper";
+import BlogHeaderWrapper from "@/components/blog/header/BlogHeaderWrapper";
+import BlogAideWrapper from "@/components/blog/aide/BlogAideWrapper";
 
 import "@/styles/base/index.css";
 import "@/styles/blog/index.css";
