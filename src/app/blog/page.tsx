6609197fa396c2d566054ebb78aa3167beb009fd
2taeyoon <2tae@kakao.com>
2026-08-05 17:32:25 +0900

    fix: update blog metadata image URL to new blog-specific favicon for improved branding consistency

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
     url: "https://www.2taeyoon.com/blog",
     images: [
       {
-        url: "https://www.2taeyoon.com/favicon/main_meta_image.png",
+        url: "https://www.2taeyoon.com/favicon/blog/blog_meta_image.png",
         alt: "Thumbnail",
       },
     ],
